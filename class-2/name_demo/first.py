c07fd7569460a8b91d1a548390309d97f878e196
Roger Huba <rogerhuba@gmail.com>
2021-12-10 22:55:08 -0600

    add demo

---
+++ class-2/name_demo/first.py
@@ -0,0 +1,11 @@
+import name_demo.root1.run_room.py
+
+
+# if __name__ == '__main__'
+# print(f'First Modules Name is {__name__}')
+
+def main():
+    print('Running from import')
+
+if __name__ == '__main__':
+    main()
