6f0eece7a28d3bdd790c3207ac13ec1d87d03897
Roger Huba <rogerhuba@gmail.com>
2022-02-01 23:36:18 -0600

    add serverless demo to class

---
+++ class_16/serverless/api/hello_world.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+
+        message = 'Hello World!' 
+        self.wfile.write(message.encode())
