9a663182d76a1d1bd620e741c4193f7a91829b0e
Roger Huba <rogerhuba@gmail.com>
2022-02-26 14:01:09 -0600

    add class demo

---
+++ class_26/django-things/things/views.py
@@ -0,0 +1,10 @@
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class InfoView(TemplateView):
+    template_name = 'info.html'
