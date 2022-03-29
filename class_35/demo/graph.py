3edce9515c2f48ab53ea3054b6a1bb399dc1bf6b
Roger Huba <rogerhuba@gmail.com>
2022-03-28 20:18:22 -0500

    add demo shell

---
+++ class_35/demo/graph.py
@@ -0,0 +1,43 @@
+class Graph:
+
+    def __init__(self):
+        pass
+
+    def add_node(self):
+        pass
+            # Arguments: value
+            # Returns: The added node
+            # Add a node to the graph
+
+    def add_edge(self):
+        pass
+            # Arguments: 2 nodes to be connected by the edge, weight (optional)
+            # Returns: nothing
+            # Adds a new edge between two nodes in the graph
+            # If specified, assign a weight to the edge
+            # Both nodes should already be in the Graph
+
+    def get_node(self):
+        pass
+            # Arguments: none
+            # Returns all nodes in graph as a collection (set, list, or similar)
+
+    def get_neighbor(self):
+        pass
+            # Arguments: node
+            # Returns a collection of edges connected to the given node
+                # Include the weight of connection in returned collection
+
+    def size(self):
+        pass
+            # Arguments: none
+            # Returns the total number of nodes in the graph
+
+
+class Vertex:
+    def __init__(self):
+        pass
+
+class Edge:
+    def __init__(self):
+        pass
---
+++ class_35/demo/test_graph.py
@@ -0,0 +1,12 @@
+# Write tests to prove the following functionality:
+
+# Node can be successfully added to the graph
+# An edge can be successfully added to the graph
+# A collection of all nodes can be properly retrieved from the graph
+# All appropriate neighbors can be retrieved from the graph
+# Neighbors are returned with the weight between nodes included
+# The proper size is returned, representing the number of nodes in the graph
+# A graph with only one node and edge can be properly returned
+# An empty graph properly returns null
+# Ensure your tests are passing before you submit your solution.
+
